411f5dcde109ec6aea4a1a09c78c38e5522988dc
Azzam <48726420+omxqn@users.noreply.github.com>
2021-12-10 00:55:59 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import { BrowserRouter } from "react-router-dom"
 import { Provider } from "react-redux"
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./redux/store"
+app.use(express.static(__dirname)); //here is important thing - no static directory, because all static :)
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 ReactDOM.render(
     <React.StrictMode>
